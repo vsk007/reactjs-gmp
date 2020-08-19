29d5c6b813611d080fd90457a75a323aaf8f110a
Vasiliy Chumak <Vasiliy_Chumak@nike.com>
2020-08-19 20:08:04 +0300

    task-2: install jest, redux, react-redux, react-router ... config babel

---
+++ homework1/movies-front/webpack.prod.js
@@ -0,0 +1,12 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config');
+
+//Plugin to minimize js code
+const TerserJSPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(common, {
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserJSPlugin()]
+    }
+});
